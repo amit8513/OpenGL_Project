b02652dde39cf47a3f228f5415453943faef77aa
amit8513 <pachzon@gmail.com>
2025-01-28 18:22:34 +0200

    Added math and soil2

--- OpenGL_Testing/Main.cpp
+++ OpenGL_Testing/Main.cpp
@@ -1,6 +1,15 @@
 #include"libs.h"
 
 int main() {
-	
+	glfwInit();
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "not working"<<"n/";
+		glfwTerminate();
+		throw "Error in GLEW INIT";
+	}
+	system("PAUSE");
 	return 0;
-}
+} 
--- OpenGL_Testing/libs.h
+++ OpenGL_Testing/libs.h
@@ -1,4 +1,12 @@
 #pragma once
+#include<glew.h>
 #include<iostream>
-
+#include<glm.hpp>
 #include<glfw3.h>
+#include<vec2.hpp>
+#include<vec3.hpp>
+#include<vec4.hpp>
+#include<mat4x4.hpp>
+#include<gtc/matrix_transform.hpp>
+#include<gtc/type_ptr.hpp>
+
